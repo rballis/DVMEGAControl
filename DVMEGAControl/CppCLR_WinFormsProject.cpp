94851a7e7dd9ab0d421369c11f0c3a550698e77d
Ralf Ballis <ralf.ballis@it-ballis.de>
2024-07-16 11:54:12 +0200

    Chilkat Unlock. Setup.

--- DVMEGAControl/CppCLR_WinFormsProject.cpp
+++ DVMEGAControl/CppCLR_WinFormsProject.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include <Windows.h>
+#include "ChilkatUnlock.h"
 using namespace System;
 
 // int main(array<System::String ^> ^args)
@@ -14,15 +15,16 @@ using namespace System::Windows::Forms;
 [STAThread]
 int main()
 {
-    const LPCWSTR name = L"DVMEGAControl";
-    HANDLE hMutex = OpenMutex(MUTEX_ALL_ACCESS, 0, name);
+    const LPCWSTR mutexName = L"DVMEGAControl";
+    HANDLE hMutex = OpenMutex(MUTEX_ALL_ACCESS, 0, mutexName);
 
-    if (!hMutex)
+    if ((!hMutex) &&
+        ((gcnew ChilkatUnlock())->Unlock()))
     {
         // Mutex doesn’t exist. This is
         // the first instance so create
         // the mutex.
-        hMutex = CreateMutex(0, 0, name);
+        hMutex = CreateMutex(0, 0, mutexName);
         Application::EnableVisualStyles();
         Application::SetCompatibleTextRenderingDefault(false);
         Application::Run(gcnew CppCLRWinFormsProject::Form1());
